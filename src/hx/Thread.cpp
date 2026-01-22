186d0699fc146cefe298774837b3427a97f88d19
Apprentice-Alchemist <53486764+Apprentice-Alchemist@users.noreply.github.com>
2026-01-22 17:11:17 +0100

    use relaxed atomics for g_nextThreadNumber

--- src/hx/Thread.cpp
+++ src/hx/Thread.cpp
@@ -297,7 +297,7 @@ Dynamic __hxcpp_thread_create(Dynamic inStart)
     #ifdef EMSCRIPTEN
     return hx::Throw( HX_CSTRING("Threads are not supported on Emscripten") );
     #else
-    int threadNumber = g_nextThreadNumber++;
+	int threadNumber = g_nextThreadNumber.fetch_add(1, std::memory_order_relaxed);
 
 	hxThreadInfo *info = new hxThreadInfo(inStart, threadNumber);
 
